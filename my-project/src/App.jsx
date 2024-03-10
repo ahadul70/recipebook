e996609e53ab52576152885663077555b477fc79
ahadul70 <ahadul70@gmail.com>
2024-03-10 09:54:09 +0600

    day 2 of 100

---
+++ my-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+
+const App = () => {
+
+
+
+  return (
+
+
+<Router>
+    <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>App</div></Router>
+
+
+
+
+  )
+}
+
+export default App
